b58b9cd2dcc4328246c762e9f3d7bb6fd2515aa0
Ricardo Coelho <devel@rmorgado.ch>
2022-03-15 08:16:19 +0100

    update

--- keycloak/keycloak-theme-vuejs-2-ts/webpack.config.js
+++ keycloak/keycloak-theme-vuejs-2-ts/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const { VueLoaderPlugin } = require("vue-loader");
-const { EnvironmentPlugin } = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
